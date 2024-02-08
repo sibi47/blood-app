de5a1c0f81d3e288b570a11c96b04a06b9819296
sibi47 <sibibabu2002@gmail.com>
2024-02-08 23:46:44 +0530

    searched

--- controller/bloodRouter.js
+++ controller/bloodRouter.js
@@ -20,4 +20,10 @@ router.get("/view",async(req,res)=>{
     res.json(data)
 })
 
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await bloodModel.find(input)
+    res.json(data)
+})
+
 module.exports=router
